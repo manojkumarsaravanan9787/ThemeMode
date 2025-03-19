5218a07116b924852662c4c4c4a11a2b0ff7743d
MANOJ KUMAR SARAVANAN <manojkumarsaravanan9787@gmail.com>
2025-03-19 19:56:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var body=document.querySelector("body");
+
+function dark(){
+    body.className="";
+    body.classList.add("dark");
+}
+
+function light(){
+    body.className ="";
+    body.classList.add("light");
+}
